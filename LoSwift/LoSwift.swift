6fe1fef2a456a3b973b97b40686909454cec8d98
= <abdullahijimoh3.ja@gmail.com>
2024-02-05 01:31:28 +0100

    configure resource folder

--- LoSwift/LoSwift.swift
+++ LoSwift/LoSwift.swift
@@ -5,7 +5,7 @@
 //  Created by Abdullah on 03/02/2024.
 //
 
-import Foundation
+import Foundation.NSBundle
 import ObjectivePGP
 import UIKit
 
@@ -29,10 +29,15 @@ final public class LoSwift: UIViewController {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
        // imageView.image = UIImage(named: "your_image_name") // Replace with your image name
-        let frameworkBundle = Bundle.module
-
+        print("asdf is \(Bundle.main.bundlePath)")
+//        let frameworkBundlePath = Bundle.main.path(forResource: "LoSwift/Resources/MyImages", ofType: "bundle")
+        let frameworkBundle = Bundle(path: "/Users/abdullah/Documents/my_apps/LoSwift/LoSwift/Resources/MyImages.bundle/")
+        print("asdf is \(frameworkBundle?.bundlePath)")
+        //let frameworkBundle = Bundle.init(path: frameworkBundlePath!)
+        
+        print("lala is \(YourFrameworkClass.bundle)")
         // Load the image from the framework bundle
-        if let image = UIImage(named: "phone-100", in: frameworkBundle, compatibleWith: nil) {
+        if let image = UIImage(named: "icons8-phone-100", in: YourFrameworkClass.bundle, compatibleWith: nil) {
             imageView.image = image
         }
         // Add the UIImageView to the view hierarchy
@@ -46,5 +51,54 @@ final public class LoSwift: UIViewController {
             imageView.widthAnchor.constraint(equalToConstant: 100), // Set your desired width
             imageView.heightAnchor.constraint(equalToConstant: 100) // Set your desired height
         ])
+    }
+}
+
+//import class Foundation.Bundle
+//private class BundleFinder {}
+//extension Foundation.Bundle {
+//    /// Returns the resource bundle associated with the current
+//    /// Swift module.
+//    static var module: Bundle = {
+//        let bundleName = "MyImages"
+//        let candidates = [
+//            // Bundle should be present here when the package is
+//            // linked into an App.
+//            Bundle.main.resourceURL,
+//            // Bundle should be present here when the package is
+//            // linked into a framework.
+//            Bundle(for: BundleFinder.self).resourceURL,
+//            // For command-line tools.
+//            Bundle.main.bundleURL,
+//        ]
+//        for candidate in candidates {
+//            let bundlePath = candidate?
+//                    .appendingPathComponent(bundleName + ".bundle")
+//            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+//                return bundle
+//            }
+//        }
+//        fatalError(
+//         "unable to find bundle ID3TagEditor_ID3TagEditorTests"
+//        )
+//    }()
+//
+//
+//}
+
+public class YourFrameworkClass {
+    static var bundle: Bundle = {
+        // This assumes that the resources are located in the same bundle as the Swift file.
+        // If your resources are in a different bundle, adjust the code accordingly.
+        let bundle = Bundle(for: YourFrameworkClass.self)
+        guard let url = bundle.url(forResource: "MyImages", withExtension: "bundle"),
+              let resourceBundle = Bundle(url: url) else {
+            return bundle
+        }
+        return resourceBundle
+    }()
+
+    public func getReferenceImage() -> UIImage? {
+        return UIImage(named: "icons8-phone-100", in: YourFrameworkClass.bundle, compatibleWith: nil)
     }
 }
